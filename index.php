7609d99a818120c1b510c1c79e1228fd560c5e52
SahabatTugas <aryasena798@gmail.com>
2021-08-20 11:28:37 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,421 @@
+<!DOCTYPE html>
+<html lang="en" class=" js no-touch">
+<head>
+  <title>Ari ♥ Shofiah | Wedding Invitation</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">  
+  <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Monda|Orbitron|Yellowtail|Open+Sans|Cookie" rel="stylesheet"> 
+  <link href="css/slick.css" rel="stylesheet" type="text/css">
+  <link href="css/slick-theme.css" rel="stylesheet" type="text/css">
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+  <link href="css/jquery.fancybox.css" rel="stylesheet" type="text/css">
+  <link href="http://i.imgur.com/DEOVT9b.png" rel="shortcut icon">
+  
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.7&appId=528754090649022";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+</head>
+<body>
+	<!--HEADER START-->
+	<row class="main-navigation" id="navbar" >
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand" href="./">Wedding Invitation</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+			  <ul class="nav navbar-nav navbar-right" style="padding-top: 5px;">
+			    <li class="active"><a href="#navbar">Home</a></li>
+			    <li><a href="#undangan">Undangan</a></li>
+			    <li><a href="#prewed">Foto</a></li>
+			    <li><a href="#lokasi">Lokasi</a></li>
+			  </ul>
+			</div>
+		  </div>
+		</nav>
+	</row>
+	<!--HEADER END-->
+	<!--BANNER START-->
+	<div id="banner">
+	<div class="inner-bg section-padding">
+		<div class="container">
+			<div class="row">
+				<div class="jumbotron col-md-8">
+					<h1 class="small">Welcome To <br /><span class="bold">Website Wedding Invitation</span></h1>
+					<h2 class="small"><span class="bold">Ari &amp; Shofiah</span></h2>
+				  <p class="big">Aku bersyukur kepada Allah yang mempertemukanku dengan pasangan hati. <br>Maka bukalah pintu rizki kami dan berkahilah kami dengannya.</p>
+				  <a href="#undangan" class="btn btn-banner">Lihat Undangan<i class="fa fa-envelope-o"></i></a>
+				  <a href="#comment" class="btn btn-banner">Ucapkan Selamat<i class="fa fa-comment-o"></i></a>
+				</div>
+				<div class="timer col-md-4" id="countdown">
+					<div class="days-wrapper"><span class="days"></span> <br>hari</div>
+					<div class="hours-wrapper"><span class="hours"></span> <br>jam</div>
+					<div class="minutes-wrapper"><span class="minutes"></span> <br>menit</div>
+					<div class="seconds-wrapper"><span class="seconds"></span> <br>detik</div>
+				</div>
+			</div>
+		  </div>
+		</div>
+	</div>
+	<!--BANNER END-->
+	<!--SERVICE START-->
+	<div id="undangan" class="section-padding">
+		<div class="container">
+			<div class="row">
+				<div class="page-title text-center">
+					<p class="cta-sub-title"><img src="http://i.imgur.com/3FTBme7.png" height="75"></p>
+					<h1>Bismillahirohmanirohim</h1>
+					<p>Maha suci Allah SWT yang telah menciptakan mahlukNya berpasang-pasangan<br>Ya Allah semoga ridhoMu tercurah mengringi pernikahan putra-putri kami. </p>
+					<hr class="pg-titl-bdr-btm"></hr>
+				</div>
+				<div class="page-title text-center">
+					<div class="nama">
+						<h2 class="small">Ari Suhari</h2>
+						<p>Putra Pertama Dari Bapak Juair & Ibu Eka Susilawati</p>
+						<h1 class="tengah">Dengan</h1>
+						<h2 class="small">Shofiah Inayati</h2>
+						<p>Putri Pertama Dari Bapak (Alm.) Hermanto & Ibu Titik Kartinah</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="service-box">
+						<div class="service-icon"><i class="fa fa-calendar"></i></div>
+						<div class="service-text">
+							<h3>Ngunduh Mantu</h3>
+							<p>Hari / Tanggal</p>
+							<p>Jum'at, 20 Agustus 2021 09.00 - Selesai WIB</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="service-box">
+						<div class="service-icon"><i class="fa fa-location-arrow"></i></div>
+						<div class="service-text">
+							<h3><a href="#lokasi">Lokasi</a></h3>
+							<p> Kediaman Mempelai Pria <br>Blok Cipedang RT 27 RW 9, Desa Mekarjati, Indramayu Jawa Barat</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="service-box">
+						
+							<p> Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir untuk memberikan do'a restu kepada kedua mempelai.</p>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--SERVICE END-->
+	<!--CTA2 START-->
+	<div class="cta-2">
+		<div class="container">
+			<div class="row text-center white">
+				<h1 class="cta-title">QS. Adz-Dzariyaat (51) : 49</h1>
+				<p class="cta-sub-title"><img src="http://i.imgur.com/JWGyO2q.png" width="750"></p>
+				<p class="cta-sub-title">“Dan segala sesuatu kami jadikan berpasang-pasangan, supaya kamu mengingat kebesaran Allah.”</p>
+			</div>
+		</div>
+	</div>
+	<!--CTA2 END-->
+	<!--GALLERY START-->
+	<div id="prewed" class="section-padding">
+		<div class="container">
+			<div class="row">
+				<div class="page-title text-center">
+					<h1>Foto Pre Wedding</h1>
+					<p>Ya Allah, jadikanlah kami pasangan yang pandai bersyukur atas apa yang kau berikan.
+					<br>Jadikanlah kami suami/istri yang saling mencintai dikala dekat,
+					<br>saling menjaga kehormatan dikala jauh, saling menghibur dikala duka.</p>
+					<hr class="pg-titl-bdr-btm"></hr>
+				</div>
+				<div class="port-sec">
+					
+					<div id="Container">
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
+							<a class="fancybox" href="Image/BeautyPlus_20210622212108691_save.jpg">
+							<img src="Image/BeautyPlus_20210622212108691_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="2">
+							<a class="fancybox" href="Image/BeautyPlus_20210623015046729_save.jpg">
+							<img src="Image/BeautyPlus_20210623015046729_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="3">
+							<a class="fancybox" href="Image/BeautyPlus_20210623015257597_save.jpg">
+							<img src="Image/BeautyPlus_20210623015257597_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="4">
+							<a class="fancybox" href="Image/BeautyPlus_20210623015558384_save.jpg">
+							<img src="Image/BeautyPlus_20210623015558384_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="5">
+							<a class="fancybox" href="Image/BeautyPlus_20210623020655723_save.jpg">
+							<img src="Image/BeautyPlus_20210623020655723_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="6">
+							<a class="fancybox" href="Image/BeautyPlus_20210623020909933_save.jpg">
+							<img src="Image/BeautyPlus_20210623020909933_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="7">
+							<a class="fancybox" href="Image/BeautyPlus_20210623021251812_save.jpg">
+							<img src="Image/BeautyPlus_20210623021251812_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
+							<a class="fancybox" href="Image/BeautyPlus_20210623021638474_save.jpg">
+							<img src="Image/BeautyPlus_20210623021638474_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="9">
+							<a class="fancybox" href="Image/BeautyPlus_20210624064544620_save.jpg">
+							<img src="Image/BeautyPlus_20210624064544620_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="10">
+							<a class="fancybox" href="Image/BeautyPlus_20210624064924673_save.jpg">
+							<img src="Image/BeautyPlus_20210624064924673_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="11">
+							<a class="fancybox" href="Image/BeautyPlus_20210624065306736_save.jpg">
+							<img src="Image/BeautyPlus_20210624065306736_save.jpg" class="img-responsive"></a>
+						</div>
+						<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="12">
+							<a class="fancybox" href="Image/BeautyPlus_20210625181418088_save.jpg">
+							<img src="Image/BeautyPlus_20210625181418088_save.jpg" class="img-responsive"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--GALLERY END-->
+	<!--PORTFOLIO START-->
+
+	<!--PORTFOLIO END-->
+	<!--CTA1 START-->
+	<div class="cta-1">
+		<div class="container">
+			<div class="row text-center white">
+				<h1 class="cta-title">QS. Yaasiin (36) : 36</h1>
+				<p class="cta-sub-title"><img src="http://i.imgur.com/Ug59JFf.png" style="width:80%"></p>
+				<p class="cta-sub-title">"Maha Suci Allah yang telah menciptakan pasangan-pasangan semuanya, baik dari apa yang ditumbuhkan oleh bumi dan dari diri mereka maupun dari apa yang tidak mereka ketahui."</p>
+			</div>
+		</div>
+	</div>
+	<!--CTA1 END-->	
+	<!--LOCATION START-->
+	<div id="lokasi" class="section-padding">
+		<div class="container">
+			<div class="row">
+				<div class="page-title text-center">
+					<h1>Lokasi Pernikahan</h1>
+					<p></p>
+					<hr class="pg-titl-bdr-btm"></hr>
+					<div class="autoplay">
+						<div class="col-md-6">
+							<h1>Kediaman Mempelai Pria</h1>
+							<p>Blok Cipedang RT 27 RW 9, Desa Mekarjati <br> Indramayu Jawa Barat</p>
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.512224297411!2d107.95952931465024!3d-6.456593764928865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6935fabb90915b%3A0x537a521a08b78b75!2sWarung%20Mama%20Eka!5e0!3m2!1sid!2sid!4v1628224388211!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--LOCATION END-->
+	<!--COMMENT START-->
+	<div id="comment" class="section-padding">
+		<div class="container">
+			<div class="row">
+				<div class="page-title text-center">
+					<h1>Ucapkan Selamat <br> Kepada kedua Mempelai</h1>
+					<p>Silahkan berikan ucapan selamat dan doa restu atas pernikahan kami <br> hubungi kami jika ada yang ingin ditanyakan dan disampaikan.</p>
+					<hr class="pg-titl-bdr-btm"></hr>
+					<div class="col-md-4"></div>
+                	<div class="col-md-4 kontak">
+						<div class="elementor-element elementor-element-5aecf580 wdp-sticky-section-no elementor-widget elementor-widget-heading" data-id="5aecf580" data-element_type="widget" data-widget_type="heading.default">
+							<div class="elementor-widget-container">
+						<p class="elementor-heading-title elementor-size-default">Kirimkan Pesan</p>		
+							</div>
+							</div>
+							<div class="elementor-element elementor-element-3b0e015c wdp-sticky-section-no elementor-widget elementor-widget-heading" data-id="3b0e015c" data-element_type="widget" data-widget_type="heading.default">
+							<div class="elementor-widget-container">
+						<p class="elementor-heading-title elementor-size-default">UNTUK KAMI BERDUA</p>
+							</div>
+							</div>
+							<div class="elementor-element elementor-element-6c58678e wdp-sticky-section-no elementor-widget elementor-widget-weddingpress-guestbook" data-id="6c58678e" data-element_type="widget" data-widget_type="weddingpress-guestbook.default">
+							<div class="elementor-widget-container">
+							
+					<div id="container">
+						<h1></h1>
+						<p></p>
+		<?php
+		if($_POST['go']){
+			mysql_connect("localhost", "root", "");
+			mysql_select_db("wedding");
+			$nama	= htmlentities(mysql_real_escape_string($_POST['nama']));
+			$pesan	= htmlentities(mysql_real_escape_string($_POST['pesan']));
+
+			if($nama && $pesan){
+				if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+					$in = mysql_query("INSERT INTO bukutamu VALUES(NULL, '$nama', '$pesan')");
+					if($in){
+						echo '<script language="javascript">alert("Terima kasih, data Anda berhasil disimpan"); document.location="index.html";</script>';}
+		?>
+						<form action="" method="post">
+							<p><b>Nama :</b><br><input type="text" name="nama" placeholder="Isikan nama anda" required /></p>
+							<p><b>Pesan :</b><br><textarea type="text" name="pesan"  rows="5" cols="30" placeholder="Tulisakan Pesan dan Doa" required></textarea></p>
+							<p><input type="submit" name="go" value="kirim Ucapan"/></p>
+						</form>
+					</div>
+						<div class="page-title text-center">
+							<br><h1>Scan QR</h1>
+							<img src="Image/barcode.jpeg" class="img-responsive"></a></br>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	<!--COMMENT END-->
+	<!--CONTACT START-->
+	<div id="kontak" class="section-padding">
+		<div class="container">
+			<div class="row">
+				<div class="page-title text-center">
+					<h1>Selamat Menempuh Hidup Baru. Semoga kalian Selalu Menjadi Keluarga Yang Berbahagia</h1>
+
+					<hr class="pg-titl-bdr-btm"></hr>
+                </div>
+			</div>
+		</div>
+	</div>
+	<a class="button-medium" href="#banner" style="margin-bottom:20px;">Home</a>
+	<!--CONTACT END-->
+
+	<!--FOOTER START-->
+	<!--footer class="footer section-padding">
+		<div class="container">
+			<div class="row">
+				<div style="visibility: visible; animation-name: zoomIn;" class="col-sm-12 text-center wow zoomIn">
+					
+				</div>
+			</div>
+		</div>
+	</footer-->
+	<!--FOOTER END-->
+	
+	<div class="footer-bottom">
+		<div class="container">
+			<div style="visibility: visible; animation-name: zoomIn;" class="col-md-12 text-center wow zoomIn">
+				<div class="footer_copyright">
+					<p> Created by <a href="https://api.whatsapp.com/send/?phone=62895395223735&text&app_absent=0">Sahabat Project</a>.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+  	<script src="js/jquery.min.js"></script>  	
+  	<script src="js/bootstrap.min.js"></script>
+  	<script src="js/jquery.mixitup.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/jquery.backstretch.min.js"></script>
+	<script src="js/countdown.js"></script>
+	<script src="js/scripts.js"></script>
+	<!--for smooth scrolling-->
+	<script>
+	$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    	}, 500);
+    	return false;
+	});
+	</script>
+<script type="text/javascript">
+	$(function(){
+		// Instantiate MixItUp:
+		$('#Container').mixItUp();
+	});
+	$('.autoplay').slick({
+		dots: true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 2,
+		slidesToScroll: 3,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+    </script>
+    <script type="text/javascript" src="js/jquery.fancybox.js"></script>
+	<script>//<![CDATA[
+	$(function(){
+	$(".fancybox")
+	    .attr('rel', 'gallery')
+	    .fancybox({
+	        padding : 0,
+	        helpers : {
+        		overlay : {
+            	css : {
+                	'background' : 'rgba(0, 0, 0, 0.7)'
+            }
+        }
+    }
+	    });
+	});//]]> 
+	</script>
+	<!-- Disable Right Click -->
+	<script type="text/javascript">
+		document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
+		$('body').on('contextmenu', 'img', function(e){ return false; });
+	</script>
+
+	<script async src="http://platform.twitter.com/widgets.js" charset="utf-8"></script>
+	<script>
+		twttr.widgets.createShareButton(
+			'https://dev.twitter.com/',
+			document.getElementById('twitter'),
+			{
+				text: 'Hello World'
+			}
+			.then( function( el ) {
+				console.log('Tweet button added.');
+			});
+		);
+	</script>
+</body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('#banner').backstretch([
+      "Image/BeautyPlus_20210623020655723_save.jpg"
+    ], {duration: 3000, fade: 750});
+});
